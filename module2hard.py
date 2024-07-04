64e9ca80b16d3632c8dfa3d40f5aa706bc6cb220
KakTys234 <168638151+KakTys234@users.noreply.github.com>
2024-07-05 00:29:16 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,23 @@
+
+def your_password(n):
+    numbers = []
+    for i in range(1, (n + 1) // 2):
+        j = i + 1
+        while i + j <= n:
+            if n % (i + j) == 0:
+                m = (i, j)
+                numbers.append(m)
+            j += 1
+    return numbers
+
+
+name = True
+while name:
+    num = int(input('Введите проверочное число для пароля: '))
+    if num in range(3, 21):
+        name = False
+    else:
+        print('Превышен предел ряда чисел от 3 до 20')
+        print('Введите число еще раз')
+password = your_password(num)
+print(f'К Вашему числу {num} подходят пароли: {password}')
